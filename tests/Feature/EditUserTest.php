adb59024cef894a38240eee675d6e25c551b1c03
Tian <leaddeveloper@aevent.com>
2021-03-24 02:44:42 -0400

    Added tests

---
+++ tests/Feature/EditUserTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class EditUserTest extends TestCase
+{
+    use RefreshDatabase;
+
+    protected $seed = true;
+
+    /**
+     * @test
+     */
+    public function test_pagination() {
+        // TODO: implement
+        $this->assertTrue(true);
+    }
+
+    /**
+     * @test
+     */
+    public function test_toggle_verified() {
+        // TODO: implement
+        $this->assertTrue(true);
+    }
+
+    /**
+     * @test
+     */
+    public function test_search() {
+        // TODO: implement
+        $this->assertTrue(true);
+    }
+
+    /**
+     * @test
+     */
+    public function test_sort() {
+        // TODO: implement
+        $this->assertTrue(true);
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_have_headers_with_correct_counting() {
+        // TODO: implement
+        $this->assertTrue(true);
+    }
+}
